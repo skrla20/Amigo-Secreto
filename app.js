484d6ff88ebc8b951e82c43b13310b120e8c111e
Karla Sanz <nechisanz@gmail.com>
2025-08-11 12:38:37 -0500

    corregir fallos

--- app.js
+++ app.js
@@ -1,65 +1,42 @@
-let nombre = [];
-console.log(nombre);
+let amigo = [];
+console.log(amigo);
 
-function agregarAmigos(nombre) {
-    if (nombre && nombre.trim() !=='') {
-        nombre.push(nombre.trim());
-        listaAmigos();
-        document.getElementById('nombre').value ='';
-    } else {
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+
+    if (nombreAmigo.trim() ==='') {
         alert('Ingrese un nombre válido');
+       } else {
+        amigo.push(nombreAmigo);
+        document.querySelector('#amigo').value ='';
+        mostrarlistaAmigos();
+        
     }
 }
 
-function listaAmigos(){
-    const lista = document.getElementById('listaAmigos');
-    lista.innerHTML =''; //limpia la lista actual
-    nombre.forEach(nombre =>{
-        const listItem = document.createElement('li');
-        listItem.textContent = nombre;
-        lista.appendChild(listItem);
-    });
+//mostramos la lista de amigos
+function mostrarlistaAmigos(){
+    let listaAmigos = document.querySelector('#listaAmigos');
+    listaAmigos.innerHTML ='';
+    
+    for (let index = 0; index <amigo.length; index++) {
+        const element = amigo[index];
+
+        let listaHtml = document.createElement('li');
+        listaHtml.textContent = element;
+        listaAmigos.appendChild(listaHtml);
+    };
 }
 
+//realizar el sorteo del amigo secreto
 function sortearAmigo () {
-    if (nombre.length <2) {
-        alert('Se neceesitan más nombres para sortear.');
+    let cantidadAmigos = amigo.length;
+    if (cantidadAmigos === 0) {
+        alert('Ingrese varios nombres para empezar el sorteo.');
+    } else{
+        let indiceAmigo = Math.floor(Math.random()*cantidadAmigos);
+        let resultadoHtml = document.querySelector('#resultado');
+        resultadoHtml.innerHTML = amigo[indiceAmigo];
         return;
-    }
-
-    const sorteo = {};
-    const shuffledNombre = [...nombre].sort(() => Math.random() -0.5);
-
-    for (let i = 0; i<nombre,length; i++) {
-        let asignado = false;
-        let intento = 0;
-        while (!asignado && intento <5) {
-            let posibleAsignado = shuffledNombre[i];
-
-            if (nombre[i] !== posibleAsignado){
-                sorteo[nombre[i]] = posibleAsignado;
-                shuffledNombre.splice(shuffledNombre.indexOf(posibleAsignado), 1);
-                asignado = true;
-            } else {
-                intento++;
-                shuffledNombre.sort(()=> Math.random() - 0.5);
-            }
-        }
-        if (!asignado) {
-            alert ('No se puede hacer el sorteo. Intente de nuevo');
-            return;
-        }
-    }
-
-    mostrarResultado(sorteo);
-}
-
-function mostrarResultado(sorteo) {
-    const resultadosDiv = document.getElementById('resultado');
-    resultadosDiv.innerHTML ='';
-    for(const nombre in sorteo) {
-        const resultadoItem = document.createElement('p');
-        resultadoItem.textContent = `${nombre} su amigo secreto es ${sorteo(nombre)}`;
-        resultadosDiv.appendChild(resultadoItem);
     }
 }
